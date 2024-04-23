32e6d7af1cf2f3c0c883c73bb1358eb25ab2eb58
prajeeshp007 <prajeeshprmod1@gmail.com>
2024-04-23 13:07:03 +0530

    first commit

---
+++ bin/dart_prajeesh1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_prajeesh1/dart_prajeesh1.dart' as dart_prajeesh1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_prajeesh1.calculate()}!');
+}
---
+++ test/dart_prajeesh1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_prajeesh1/dart_prajeesh1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
